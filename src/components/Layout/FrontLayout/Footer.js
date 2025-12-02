a79e3b34e079d3c0a37398c319bf63d1f4450ac6
Scott Stebleton <scott@stebleton.net>
2025-12-02 16:42:08 -0500

    add farewell text to index

--- src/components/Layout/FrontLayout/Footer.js
+++ src/components/Layout/FrontLayout/Footer.js
@@ -1,32 +1,14 @@
-import {useRouter} from "next/router";
-
 import ColorModeToggler from "../../common/ColorModeToggler/ColorModeToggler";
 
 import classes from './Footer.module.scss';
 
-const Footer = () => {
-  const router = useRouter();
-
-  return (
-    <div className={classes.Footer}>
-      <a href={'/director'} className={`${classes.Element}`}>
-        Login
-      </a>
-      {router.pathname !== '/about' &&
-        <a href={'/about'} className={`${classes.Element}`}>
-          About
-        </a>
-      }
-      <span className={`${classes.Element}`}>
-        &copy; 2015-2024
-        {' '}
-        <a href={'mailto:hello@tourn.io'}>
-          Tournio
-        </a>
-      </span>
-      <ColorModeToggler className={`d-inline-block mt-1 mb-0 py-1`} />
-    </div>
-  );
-}
+const Footer = () => (
+  <div className={classes.Footer}>
+    <span className={`${classes.Element}`}>
+      &copy; 2015-2025 Tourn.io
+    </span>
+    <ColorModeToggler className={`d-inline-block mt-1 mb-0 py-1`} />
+  </div>
+);
 
 export default Footer;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,92 @@
-import FrontLayout from '../components/Layout/FrontLayout/FrontLayout';
-import Front from "../components/Front/Front";
+import Image from "next/image";
 
-const IndexPage = () => {
+import {useThemeContext} from "../store/ThemeContext";
+import logoImage from "../images/tournio-logo.png";
+import logoImageDark from "../images/tournio-logo-inverted-gray.png";
+
+import FrontLayout from '../components/Layout/FrontLayout/FrontLayout';
+import {useClientReady} from "../utils";
+
+const Page = () => {
+  const {theme} = useThemeContext();
+
+  const ready = useClientReady();
+  if (!ready) {
+    return null;
+  }
+
   return (
-    <Front/>
+    <div>
+      <div className="row offset-lg-3 col-lg-6">
+        <div className="pb-2 pb-md-0">
+          <div className="mb-3 position-relative">
+            {theme.active === 'light' &&
+              <Image src={logoImage}
+                     alt={"Tourn.io logo"}
+                     width={800}
+                     height={169}
+                     sizes={'100vw'}
+                     style={{
+                       width: '100%',
+                       height: 'auto',
+                     }}
+              />
+            }
+            {theme.active === 'dark' &&
+              <Image src={logoImageDark}
+                     alt={"Tourn.io logo"}
+                     width={800}
+                     height={166}
+                     sizes={'100vw'}
+                     style={{
+                       width: '100%',
+                       height: 'auto',
+                     }}
+              />
+            }
+          </div>
+        </div>
+      </div>
+      <div className="row offset-lg-3 col-lg-6">
+        <h1>Thanks for ten great years!</h1>
+        <p>
+          Tourn.io was a registration system for IGBO bowling tournaments that aimed to be easy to use for both bowlers and committee members alike, with easy setup, testing, registration, administration, and reporting; plus, in later years, a reliable payments integration via Stripe.
+        </p>
+        <p>
+          From the start, Tourn.io was intended as a bridge from a previous system (or no system at all, in the case of some tournaments) to a new one provided by IGBO for all its member tournaments to use. That system now exists. So, even though it&apos;s hard to say goodbye, Tourn.io has fulfilled its purpose and will remain closed indefinitely.
+        </p>
+        <hr />
+        <p>
+          Building and maintaining this application was a labor of love, but it was also a way for me to keep my technical skills sharp, both after I took on a leadership role at work, and after I stopped working due to a life-changing illness and permanent disability.
+        </p>
+        <p>
+          I want to issue a heartfelt <strong>thank you</strong> to all the tournament directors I got to work with over the past decade, and even those I didn&apos;t get to work with. It&apos;s been a fantastic learning experience for me, and I hope the feeling is mutual. I want to especially call out the directors of the
+          <a className={"mx-1"} href={"https://www.bigdclassic.com/"}>
+            Big D Classic
+          </a>
+          tournament, who graciously let me try out the first version on them.
+        </p>
+        <p>
+          Here&apos;s to IGBO growing and continuing to provide a safe space for queer bowlers everywhere!
+        </p>
+        <p>
+          In fellowship,
+          <br />
+          <a href={"mailto:scott@stebleton.net"}>
+            Scott Stebleton
+          </a>
+        </p>
+      </div>
+    </div>
   );
 }
 
-IndexPage.getLayout = function getLayout(page) {
+Page.getLayout = function getLayout(page) {
   return (
-    <FrontLayout>
+    <FrontLayout showHomeLink={true}>
       {page}
     </FrontLayout>
   );
 }
 
-export default IndexPage;
+export default Page;
